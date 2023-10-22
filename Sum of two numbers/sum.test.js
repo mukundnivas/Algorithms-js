5416f7dda9c9426d028ee146c8ab860b75420e77
mukund <mukundnivas@gmail.com>
2023-10-22 13:21:28 +1100

    added more tests to check add function

--- Sum of two numbers/sum.test.js
+++ Sum of two numbers/sum.test.js
@@ -3,3 +3,11 @@ const sum = require("./sum");
 test("add 1 and 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("add 1 and -2 to equal -1", () => {
+  expect(sum(1, -2)).toBe(-1);
+});
+
+test("add with no arguments", () => {
+  expect(sum()).toBeNaN();
+});
