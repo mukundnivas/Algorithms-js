98b4bd8cc50a50afc478041e55ea9c5db051454c
mukund <mukundnivas@gmail.com>
2023-11-01 21:05:27 +1100

    Fibbonaci sequence and tests

--- Fibbonaci Sequence/fibbonaci.js
+++ Fibbonaci Sequence/fibbonaci.js
@@ -1,5 +1,11 @@
 const fibbonaci = (n) => {
-  return n;
+  const fib = [0, 1];
+
+  for (let i = 2; i <= n - 1; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib;
 };
 
 module.exports = fibbonaci;
--- Fibbonaci Sequence/fibbonaci.test.js
+++ Fibbonaci Sequence/fibbonaci.test.js
@@ -1,9 +1,9 @@
 const fibbonaci = require("./fibbonaci");
 
 test("Fibbonaci of 5", () => {
-  expect(fibbonaci(5)).toBe(5);
+  expect(fibbonaci(5)).toStrictEqual([0, 1, 1, 2, 3]);
 });
 
 test("Fibbonaci of 9", () => {
-  expect(fibbonaci(9).toBe(34));
+  expect(fibbonaci(9)).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13, 21]);
 });
