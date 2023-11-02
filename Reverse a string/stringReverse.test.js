0bac70cecaf508343dce4b6d4509743d0b0f3561
mukund <mukundnivas@gmail.com>
2023-11-02 12:07:06 +1100

    added unit test to String Reverse

--- Reverse a string/stringReverse.test.js
+++ Reverse a string/stringReverse.test.js
@@ -3,3 +3,7 @@ const stringReverse = require("./stringReverse");
 test("Reverse the string 'paper'", () => {
   expect(stringReverse("paper")).toBe("repap");
 });
+
+test("Reverse the string 'automation'", () => {
+  expect(stringReverse("automation")).toBe("noitamotua");
+});
