455fd58614b5de678e98c45cdc75c2eeb7ed059f
mukund <mukundnivas@gmail.com>
2023-11-02 17:54:55 +1100

    Linear search and tests

--- Warm up problems/Linear search/isLinearSearch.js
+++ Warm up problems/Linear search/isLinearSearch.js
@@ -1,5 +1,8 @@
-const isLinearSearch = (a) => {
-  return true;
+const isLinearSearch = (searchArray, element) => {
+  for (let i = 0; i < searchArray.length; i++) {
+    if (searchArray[i] === element) return true;
+  }
+  return false;
 };
 
 module.exports = isLinearSearch;
--- Warm up problems/Linear search/isLinearSearch.test.js
+++ Warm up problems/Linear search/isLinearSearch.test.js
@@ -1,5 +1,9 @@
 const isLinearSearch = require("./isLinearSearch");
 
-test("Searching via Linear Search if '2' is present in the string [41,42,1,86,2,31,52]", () => {
-  expect(isLinearSearch([41, 42, 1, 86, 2, 31, 52])).toBe(true);
+test("Searching via Linear Search if '2' is present in the array [41,42,1,86,2,31,52]", () => {
+  expect(isLinearSearch([41, 42, 1, 86, 2, 31, 52], 2)).toBe(true);
+});
+
+test("Searching via Linear Search if '34' is present in the string [42,23,21,586,642,31,52]", () => {
+  expect(isLinearSearch([42, 23, 21, 586, 642, 31, 52], 32)).toBe(false);
 });
